f2fe6f350c1b43cc56df08dd184ddc5b111304a1
jws909 <jws090909@gmail.com>
2026-06-29 12:42:14 +0900

    마이페이지 오류 수정 및 userData에 자기소개문구 추가

--- src/data/accountData.js
+++ src/data/accountData.js
@@ -1,19 +1,21 @@
-
 let USERS = [
     {
         name: "춘식이",
         email: "chunsik@gmail.com",
         password: "1111",
+        bio: "느긋하게 사는 게 인생 목표인 고양이"
     },
     {
         name: "라이언",
         email: "lion@gmail.com",
         password: "1111",
+        bio: "무표정하지만 속은 따뜻한 수사자"
     },
     {
         name: "어피치",
         email: "apeach@gmail.com",
         password: "1111",
+        bio: "복숭아지만 탈출을 꿈꾸는 캐릭터"
     },
 ]
 
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,10 +2,12 @@ import '../css/MyPage.css'
 
 import { Person, PencilSquare } from 'react-bootstrap-icons';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import useAccountStore from '../store/useAccountStore'
 
 
 function MyPage() {
 
+    const currentUser = useAccountStore((s) => s.user);
 
     return (
         <div className="mypage-wrapper">
@@ -23,12 +25,12 @@ function MyPage() {
                     <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                     </svg> choonsik@teamco.com</span><br />
-                    <span className='mypage-post'><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                    <span className='mypage-post'><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-file-earmark-text" viewBox="0 0 16 16">
                         <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5" />
                         <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
                     </svg> 내가 작성한 글: 3개</span>
                     {' '}
-                    <span className='mypage-post' style={{margin:'8px'}}><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
+                    <span className='mypage-post' style={{ margin: '8px' }}><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-chat-left" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                     </svg> 내가 작성한 댓글: 5개</span>
 
@@ -44,18 +46,16 @@ function MyPage() {
                 <div className='mypage-update'>
                     <p>프로필 수정</p>
                     <p>닉네임</p>
-                    <input type="text" value="춘식이" />
+                    <input type="text" defaultValue="춘식이" />
                     <p>자기소개</p>
-                    <textarea>팀 프로젝트 열심히 합시다!</textarea>
+                    <textarea defaultValue="팀 프로젝트 열심히 합시다!" />
 
                 </div>
 
                 <div className='mypage-btn'>
-                    <button type='button' style={{ backgroundColor: '#969696', width:'60px' }}>취소</button>
+                    <button type='button' style={{ backgroundColor: '#969696', width: '60px' }}>취소</button>
                     <button type='submit'>변경사항 저장</button>
                 </div>
-
-
 
             </form>
 
